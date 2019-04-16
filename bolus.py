e90d0bfd9ac66f9875d675539d0ca5a89a4053a2
NerdHackBots <48294732+NerdHackBots@users.noreply.github.com>
2019-04-16 16:07:16 -0700

    Update bolus.py

--- bolus.py
+++ bolus.py
@@ -5,33 +5,33 @@ Created on Fri Apr 12 07:44:38 2019
 
 @author: NerdHackBots
 """
+
+#analysis
 class insulin(object):
     bolus = 0
-    
     
     def __init__(self, name, sugar_optimal, correction_factor, carb_ratio, insulin_on_board):
         self.name = name
         self.sugar_optimal = sugar_optimal
         self.correction_factor = correction_factor
         self.carb_ratio = carb_ratio
+        self.insulin_on_board = insulin_on_board
     
+    '''
     def insulin_on_board():
         pass
+    '''
     
     def correction_insulin(self, sugar_current):
         if sugar_current > self.sugar_optimal:
             insulin_correct = (sugar_current - self.sugar_optimal) / self.correction_factor
-           
-            '''
-            if insulin_correct > insulin_on_board:
-                insulin_correct = insulin_correct
-            else:
+            if self.insulin_on_board > insulin_correct:
                 insulin_correct = 0
-            '''
-            
-            return insulin_correct
+            else:
+                insulin_correct = insulin_correct - self.insulin_on_board
         else:
-            pass
+            insulin_correct = 0
+        return insulin_correct
     
     def carb_insulin(self, carb_amount):
         insulin_bolus = self.carb_ratio * carb_amount
@@ -41,5 +41,6 @@ class insulin(object):
         insulin_total = self.insulin_bolus + self.insulin_correct
         return insulin_total
     
-    def print_insulin(self):
-        print("Delivering " + self.insulin_total + " units of insulin")
+    def deliver_insulin(self):
+        while self.insulin_total != 0:
+            print(self.insulin_total)
